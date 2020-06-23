7ab1052d04d764f64ab400efae86d423fe8b5de7
Roman Turovskyy <roman.turovskyy@eleks.com>
2020-06-23 22:57:10 +0300

    initial

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    redisHost: process.env.REDIC_HOST,
+    redisPort: process.env.REDIC_PORT,
+    pgUser: process.env.PGUSER,
+    pgHost: process.env.PGHOST,
+    pgDatabase: process.env.PGDATABASE,
+    pgPassword: process.env.PGPASSWORD,
+    pgPort: process.env.PGPORT
+};
---
+++ worker/keys.js
@@ -0,0 +1,4 @@
+module.export = {
+    redisHost: process.env.REDIC_HOST,
+    redisPort: process.env.REDIC_PORT
+};
